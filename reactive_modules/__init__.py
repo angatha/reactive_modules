7fe640586a4770a2a9aa0bfd37782af18d72db0a
Jonas Eckert <j.eckert@ostfalia.de>
2022-03-23 08:12:12 +0100

    Fix version reference

--- reactive_modules/__init__.py
+++ reactive_modules/__init__.py
@@ -1,4 +1,3 @@
-from version import version
 from .cst import Module, Atom, ReferenceExpression, Expression, ConstantExpression, ValueExpression, GuardedCommand, \
     BooleanExpression, ExecutionContext, VariableValue, BooleanDomain, Domain, DefinedDomain, DefinedNumericDomain, \
     KnownDomain, RealDomain, IntegralDomain, NaturalDomain, TypeException, SourceDescriptor, EvaluationException, \
@@ -8,5 +7,6 @@ from .formatting import format_str
 from .parser import ParseException, parse, parse_module, parse_atom
 from .simulation import SimulationController, EndlessController, InteractiveController, SimulationStateObserver, \
     ObserverGroup, IOStateLogger, HistoryLogger, LogEntry, LogEntryPart, ExecutionException, simulate_module
+from .version import version
 
 __version__ = version
